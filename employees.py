24cbc2b5290d482fdba3ee93624a40da34fcd689
Abigail <abigailwicks@yahoo.com>
2017-02-11 09:47:17 -0800

    other tutor practice 2/11

---
+++ employees.py
@@ -0,0 +1,27 @@
+employee = {}
+name = raw_input("What is your name? ")
+age = int(raw_input("How old are you? "))
+height = int(raw_input("How tall are you, in inches? "))
+
+employee["name"] = name
+employee["age"] = age
+employee["height"] = height
+# employee["height"] = 60
+# employee.get("name")
+# print employee.get("name", "Plese enter your name again")
+
+# print employee["name"]
+# print employee["age"]
+# print employee["height"]
+# print employee.get(26, "It doesn't exist") #mydict.get(key, "DNE")
+# for my_key, my_value in employee.items():
+# 	print my_key, my_value
+# for key in employee:
+# 	print key
+# 	print employee[key]
+# for key in employee:
+# 	print key, employee[key]
+# for key in employee:
+# 	print key
+# 	print employee.get(key)
+
