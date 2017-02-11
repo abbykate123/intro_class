f3bc1a87038dad05d1c9f92aa7c761584df79678
Abigail <abigailwicks@yahoo.com>
2017-02-11 09:39:10 -0800

    tutor practice 2/11

---
+++ tuples_dictionary.py
@@ -0,0 +1,37 @@
+# hiking_trails = {
+# 	'Berkeley' : ['Tilden Park'],
+# 	'Yosemite' : ['Half Dome', 'Clouds Rest'],
+# 	'Marin' : []
+# 	}
+# hiking_trails["San Francisco"] = ["Land's End"]
+# hiking_trails["Yosemite"] = ['Half Dome', 'Clouds Rest', 'Pohono Trail'] 
+# for my_key, my_value in hiking_trails.items():
+# 	print my_key, my_value
+
+# prices = {
+# 	"banana": 4, "apple":2, "orange":1.5, "pear":3
+# }
+# maxprice = -100
+# for key in prices:
+# 	price = prices[key]
+# 	if(price > maxprice):
+# 		maxprice = price
+
+# for key in prices:
+# 	price = prices[key] 
+# 	if (price == maxprice):
+# 		print key
+
+# def prime(number):
+# 	isprime = True
+# 	if number == 1:
+# 		isprime = False
+# 	for count in range(2, number):
+# 		if number % count == 0:
+# 			isprime = False
+
+# 	return isprime
+
+# print prime(1)
+
+# optimize function to work with big numbers
